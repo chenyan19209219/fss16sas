445d2f88508884101ef2e4e61a232c4c0312292c
ankur57k <ankur57k@gmail.com>
2016-10-04 21:31:29 -0400

    Ankur WS2 Screenshot

--- code/WS2/ankur.py
+++ code/WS2/ankur.py
@@ -4,12 +4,11 @@ import random
 # TODO 1: Fill in your unity ID
 __name__ = "akatari2"
 
-
 def kv(d):
     """
     Pretty Print the dictionary.
     """
-    return '(' + ','.join(['%s: %s' % (k, d[k]) for k in sorted(d.keys()) if k[0] != "_"]) + ')'
+    return '(' + ','.join(['%s: %s'%(k, d[k]) for k in sorted(d.keys()) if k[0] != "_"]) + ')'
 
 
 def shuffle(lst):
@@ -19,24 +18,21 @@ def shuffle(lst):
     random.shuffle(lst)
     return lst
 
-
 ## Function to get the random value between a lower and upper bound.
 randint = random.randint
-
 
 class O(object):
     """
     Basic Class which every other class inherits
     """
-
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
     def __repr__(self):
         return self.__class__.__name__ + kv(self.__dict__)
 
+print("Hello %s"%__name__)
 
-print("Hello %s" % __name__)
 
 
 class State(O):
@@ -44,7 +40,6 @@ class State(O):
     State object
     """
     visit_limit = 5
-
     def __init__(self, name):
         """
         Initialize a state.
@@ -54,7 +49,7 @@ class State(O):
             out: List of transitions
             visits: Number of times the state was visited if not declared as a loop
         """
-        O.__init__(self, name=name, out=[], visits=0)
+        O.__init__(self, name=name, out=[], visits = 0)
 
     def is_stop(self):
         """
@@ -95,7 +90,6 @@ class Trans(O):
         """
         O.__init__(self, here = here, guard = guard, there = there)
 
-
 class Machine(O):
     def __init__(self, label, data=0):
         """
@@ -103,10 +97,10 @@ class Machine(O):
         @param label: Label representing the machine
         @param data: Data used to describe the machine. In case of this world model, data represents energy
         """
-        O.__init__(self, label=label,  # Label of the machine
-                   states={},  # Possible state of the machine
-                   here=None,  # Current state of the machine
-                   data=data)  # Data used to describe the machine
+        O.__init__(self, label = label, # Label of the machine
+                   states = {}, # Possible state of the machine
+                   here = None, # Current state of the machine
+                   data = data) # Data used to describe the machine
 
     def add_state(self, name):
         # TODO 2:
@@ -114,42 +108,37 @@ class Machine(O):
         # Create an instance of state, add it to the states map. Also if
         # the current state is None, set it to this state.
         # Also return the state
-
-        current_state = State(name)
-        self.states[name] =  current_state
+        st = State(name)
+        self.states[name] = st
         if self.here is None:
-            self.here = current_state
-        return current_state
+            self.here = st
+        return st
 
     def add_trans(self, *trans):
         # TODO 3: For every transition in the list *trans, add the
-        # transition to the "out" list in its "here" state
+        # transition to the "out" list in the "here" state
         for tran in trans:
             tran.here.out.append(tran)
 
-
     def step(self):
         # TODO 4: Move the machine to the next state if it is currently not in the stop state.
-        if self.here.is_stop is False:
+        if not self.here.is_stop():
             self.here = self.here.next(self)
-            self.here.arrive()
-
 
 class Factory(O):
     """
     Factory that generates machines.
     """
-
     def __init__(self):
         """
         Initialize the factory.
         """
-        O.__init__(self, machines=[])
+        O.__init__(self, machines = [])
 
     def make_machine(self, label, data=0):
         # TODO 5: Create a new machine and add it to
         # the list "machines" and return the machine
-        machine = Machine(label, data)
+        machine = Machine(label,data)
         self.machines.append(machine)
         return machine
 
@@ -174,16 +163,15 @@ class Factory(O):
         Report the runs
         """
         max_len = 50
-        lst = [0] * (max_len + 1)
+        lst = [0]*(max_len + 1)
         for machine in self.machines:
             lst[machine.data] += machine.label
         show = lambda x: str(x if x else '.')
         print(name, " | ", " ".join(map(show, lst)))
 
-
-grass_chance = 0.5
-pit_chance = 0.5
-
+snow_chance = 0.5
+grass_chance = 1
+pit_chance = 0.1
 
 def snow(m, t):
     """
@@ -194,11 +182,10 @@ def snow(m, t):
     """
     # TODO 6: If chance < snow_chance, reduce the machine's energy
     # to a random integer between [1, 5] and return True. Else return False
-    if random.random < snow_chance:
-        s.data -= randint(1, 5)
+    if random.random() <snow_chance:
+        m.data -= randint(1,5)
         return True
     return False
-
 
 def grass(m, t):
     """
@@ -208,11 +195,10 @@ def grass(m, t):
     """
     # TODO 7: If chance < grass_chance, increase the machine's energy
     # to a random integer between [1, 5] and return True. Else return False
-    if random.random < snow_chance:
-        s.data += randint(1, 5)
+    if random.random() <grass_chance:
+        m.data += randint(1,5)
         return True
     return False
-
 
 def pit(m, t):
     """
@@ -234,20 +220,17 @@ def fsm(factory, label, data):
     m = factory.make_machine(label, data)
     # TODO 8: Using the functions and classes defined above code up the
     # state machine in the figure at the top of the page.
-
     start = m.add_state("start")
     outside = m.add_state("#outside")
-    end = m.add_state("dead.")
-    m.add_trans(Trans(start, walk, outside),
-                 Trans(outside, grass, outside),
-                 Trans(outside, snow, outside),
-                 Trans(outside, pit, end), )
+    dead = m.add_state("dead.")
+    m.add_trans(Trans(start,walk,outside),
+                Trans(outside,grass,outside),
+                Trans(outside,snow,outside),
+                Trans(outside,walk,dead))
     return m
-
 
 f = Factory()
 fsm(f, 1, 25)
 fsm(f, 2, 25)
 fsm(f, 4, 25)
 f.run(100)
-# TODO 9 : Suggest an upgrade to the model by adding more transitions/states and check out the results
